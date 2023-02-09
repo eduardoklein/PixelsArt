af1cf753fd4a918500d7c3617959cdc75eb11e8b
Eduardo Klein <eduardo.klein@live.com>
2023-02-08 22:59:42 -0300

    Requisito 5 finalizado

--- script.js
+++ script.js
@@ -8,14 +8,30 @@ function colorChange() {
         for (i = 1; i <=6; i += 1){
             colorRandom[i] = colorHex[Math.floor(Math.random() * 16)];
         }
-        console.log(randomColor[index]);
-        console.log(colorRandom.join(''));
         randomColor[index].style.backgroundColor = colorRandom.join('');
-    }   
-}   
-
- buttonColorChange.addEventListener('click', colorChange);
-
-function salvarLocalStorage() {
-    localStorage.setItem('color-palette', randomColor);
+    }  
+    saveInLocalStorage();
 }
+
+buttonColorChange.addEventListener('click', colorChange);
+
+function saveInLocalStorage(){
+    const color1 = randomColor[1].style.backgroundColor;
+    const color2 = randomColor[2].style.backgroundColor;
+    const color3 = randomColor[3].style.backgroundColor;
+    localStorage.setItem('colorPalette', JSON.stringify([color1, color2, color3]));
+}
+
+function recoveryPalette(){
+    const recoveryParams = JSON.parse(localStorage.getItem('colorPalette'));
+    if (recoveryParams != null) {
+        randomColor[1].style.backgroundColor = recoveryParams[0];
+        randomColor[2].style.backgroundColor = recoveryParams[1];
+        randomColor[3].style.backgroundColor = recoveryParams[2];
+    } 
+ 
+
+}
+
+window.addEventListener('load', recoveryPalette);
+
