2ecd6ba93b0a93eb92d3f965ceef43a892c9581e
Eduardo Klein <eduardo.klein@live.com>
2023-02-09 23:41:14 -0300

    Finalizando o requisito 10

--- script.js
+++ script.js
@@ -2,14 +2,13 @@ const randomColor = document.querySelectorAll('.color');
 const buttonColorChange = document.getElementById('button-random-color');
 const coluna = document.getElementById('coluna');
 const pixel = document.getElementById('pixel');
+let colorSelected = document.querySelectorAll('.color');
 const pixelBoard = document.getElementById('pixel-board');
-const defaultBlack  = document.querySelector('.preto');
 const colorPalette = document.querySelector('#color-palette');
-const colorSelected = document.querySelectorAll('.color');
+let colorPaint = '#000';
 
 
-
-
+// Random color
 function colorChange() {
     let colorHex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
     let colorRandom = ['#'];
@@ -23,16 +22,18 @@ function colorChange() {
 }
 buttonColorChange.addEventListener('click', colorChange);
 
-
+// Saving in localStorage
 function saveInLocalStorage(){
     const color1 = randomColor[1].style.backgroundColor;
     const color2 = randomColor[2].style.backgroundColor;
     const color3 = randomColor[3].style.backgroundColor;
     localStorage.setItem('colorPalette', JSON.stringify([color1, color2, color3]));
 }
+// Recovering from localStorage
 function recoveryPalette(){
     const recoveryParams = JSON.parse(localStorage.getItem('colorPalette'));
     if (recoveryParams != null) {
+        randomColor[0].style.backgroundColor = '#000';
         randomColor[1].style.backgroundColor = recoveryParams[0];
         randomColor[2].style.backgroundColor = recoveryParams[1];
         randomColor[3].style.backgroundColor = recoveryParams[2];
@@ -49,24 +50,23 @@ for (index = 1; index <= 5; index += 1){
     for (i = 1; i <= 5; i += 1){
         const pixel = document.createElement('div');
         pixel.classList.add('pixel');
+        pixel.addEventListener('click', (event) =>{
+            event.target.style.backgroundColor = colorPaint;
+        })
         coluna.appendChild(pixel);
     }
     pixelBoard.appendChild(coluna);
 }
 
-function selecionarPreto(event) {
-    defaultBlack.classList.add('selected')
-}
-window.addEventListener('load', selecionarPreto);
-
 function selectedColor (event){
-    if (colorSelected[0].classList.contains('selected') || colorSelected[1].classList.contains('selected') || colorSelected[2].classList.contains('selected') ||colorSelected[3].classList.contains('selected')  ){
+    if (colorSelected[0].classList.contains('selected') || colorSelected[1].classList.contains('selected') || colorSelected[2].classList.contains('selected') || colorSelected[3].classList.contains('selected')){
         colorSelected[0].classList.remove('selected');
         colorSelected[1].classList.remove('selected')
         colorSelected[2].classList.remove('selected')
         colorSelected[3].classList.remove('selected')
     }
     event.target.classList.add('selected');
+    colorPaint = event.target.style.backgroundColor;    
 }
 colorSelected[0].addEventListener('click', selectedColor);
 colorSelected[1].addEventListener('click', selectedColor);
@@ -74,3 +74,4 @@ colorSelected[2].addEventListener('click', selectedColor);
 colorSelected[3].addEventListener('click', selectedColor);
 
 // https://www.javascripttutorial.net/dom/css/check-if-an-element-contains-a-class/
+
